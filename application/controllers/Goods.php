8ec9c58bbbe534c564c8a684cac1729d4245ed2f
luxiaofeng <luxiaofeng.200@163.com>
2018-11-23 09:47:56 +0800

    优化

--- application/controllers/Goods.php
+++ application/controllers/Goods.php
@@ -1,5 +1,11 @@
 <?php
+/**
+* @note 测试方法
+*/
+
 class GoodsController extends Yaf_Controller_Abstract {
+
+
     public function detailAction(){
         echo __METHOD__;
         die;
@@ -12,6 +18,12 @@ class GoodsController extends Yaf_Controller_Abstract {
     public function testAction(){
     	echo __METHOD__;
     	echo 333;exit;
+    }
+
+    public function initMyReductAction(){
+       echo __METHOD__;
+       echo "<br/>";
+        return false;
     }
 }
 ?>
